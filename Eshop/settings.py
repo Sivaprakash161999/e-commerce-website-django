3388498328beb1e10c1a8e7d35f8d24034ce7554
Sivaprakash161999 <sivaprakash161999@gmail.com>
2023-03-10 01:09:24 +0530

    e-commerce website final code and resources

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "store",
 ]
 
 MIDDLEWARE = [
@@ -110,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = "/image/download/"
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models.product import Products
+from .models.category import Category
+from .models.customer import Customer
+from .models.orders import Order
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
 
 # Register your models here.
+admin.site.register(Products, AdminProduct)
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+
+
+
+
+# username = siva, email = siva@siva.com, password = siva
